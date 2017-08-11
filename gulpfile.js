9957e90a05248e3a7d98fd25a5e1c5ebc917865e
tooleks <tooleks@gmail.com>
2017-08-12 01:39:23 +0300

    Implemented macro calls

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,29 @@
 "use strict";
 
-var fs = require("fs");
-var gulp = require("gulp");
+const fs = require("fs");
+const gulp = require("gulp");
+const replace = require("gulp-replace");
+const macro = require("./macro");
 
-gulp.task("build", function () {
-    var rules = [];
+gulp.task("build", () => {
+    let rules = [];
     // Load and concatenate all rules from rules directory.
-    fs.readdirSync("./rules").forEach(function (file) {
-        rules = rules.concat(require("./rules/" + file));
-    });
+    fs.readdirSync("./rules/").forEach((file) => rules = rules.concat(require("./rules/" + file)));
     // Sort rules by priority descending.
-    rules.sort(function (firstRule, secondRule) {
-        return secondRule.priority - firstRule.priority;
-    });
+    rules.sort((firstRule, secondRule) => secondRule.priority - firstRule.priority);
     // Write rules into single file.
-    fs.writeFile("./dist/rules.json", JSON.stringify(rules), function (error) {
-        if (error) {
-            throw error;
-        }
+    fs.writeFile("./dist/rules.json", JSON.stringify(rules), (error) => {
+        if (error) throw error;
+        gulp.start("replace:macro");
     });
+});
+
+gulp.task("replace:macro", () => {
+    const task = gulp.src(["./dist/**/*.json"]);
+    for (let macroName in macro) {
+        if (macro.hasOwnProperty(macroName)) {
+            task.pipe(replace(macroName, macro[macroName]));
+        }
+    }
+    task.pipe(gulp.dest("./dist/"));
 });
