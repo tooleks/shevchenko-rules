f9e881d6ee95e771cf5b98cd4a7df8a6038e5a16
tooleks <tooleks@gmail.com>
2020-04-20 15:27:15 +0300

    Refactored rule structure, upgraded dependencies, updated README.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const gulp = require("gulp");
-const replace = require("gulp-replace");
-const macro = require("./macro");
+const util = require('util');
+const fs = require('fs');
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const macro = require('./macro');
 
-gulp.task("build", () => {
-    let rules = [];
-    fs.readdirSync("./src/").forEach((file) => rules = rules.concat(require("./src/" + file)));
-    rules.sort((firstRule, secondRule) => secondRule.priority - firstRule.priority);
-    fs.writeFile("./dist/rules.json", JSON.stringify(rules), (error) => {
-        if (error) throw error;
-        gulp.start("replace:macro");
-    });
+const readdirAsync = util.promisify(fs.readdir);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+gulp.task('bundle:rules', async () => {
+  const fileNames = await readdirAsync('./src');
+  // Concatenate all rules into a single array.
+  const rules = fileNames
+    .reduce((rules, fileName) => rules.concat(require('./src/' + fileName)), [])
+    .sort((firstRule, secondRule) => secondRule.priority - firstRule.priority);
+  await writeFileAsync('./dist/rules.json', JSON.stringify(rules));
 });
 
-gulp.task("replace:macro", () => {
-    const task = gulp.src(["./dist/**/*.json"]);
-    for (let macroName in macro) {
-        if (macro.hasOwnProperty(macroName)) {
-            task.pipe(replace(macroName, macro[macroName]));
-        }
-    }
-    task.pipe(gulp.dest("./dist/"));
+gulp.task('replace:macro', () => {
+  const task = gulp.src('./dist/**/*.json');
+  // Replace all macro expressions in destination files.
+  Object.getOwnPropertyNames(macro).forEach((macroName) => {
+    task.pipe(replace(macroName, macro[macroName]));
+  });
+  return task.pipe(gulp.dest('./dist'));
 });
+
+gulp.task('build', gulp.series('bundle:rules', 'replace:macro'));
